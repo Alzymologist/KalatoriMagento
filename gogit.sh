019f6fa482744746fa2ecbfa370683364f52b534
Leonid Kaganov <lleo@lleo.me>
2024-03-02 15:20:40 +0200

    Saturday work with Max 2

---
+++ gogit.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# change .git/config:
+#        #url = https://github.com/Alzymologist/wasm-js.git
+#        url = git@github.com:Alzymologist/wasm-js.git
+
+#git checkout -b better-init
+#git push
+#exit
+
+#git clone git@github.com:Alzymologist/shave-rust.git
+
+git status
+git add .
+git commit -m "Saturday work with Max 2"
+git push
+
+
+# git@github.com:lleokaganov/Kalatori-frontend-plugins.git
