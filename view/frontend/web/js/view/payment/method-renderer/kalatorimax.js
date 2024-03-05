613b2f59c88d69fa60925886aec0b29acf699542
Maksim Soldatjonok <maksold@gmail.com>
2024-03-05 16:10:49 +0200

    Add selectPaymentMethod to kalatorimax.js

--- view/frontend/web/js/view/payment/method-renderer/kalatorimax.js
+++ view/frontend/web/js/view/payment/method-renderer/kalatorimax.js
@@ -1,13 +1,43 @@
 define(
     [
-        'Magento_Checkout/js/view/payment/default'
+        'jquery',
+        'Magento_Checkout/js/view/payment/default',
+        'Magento_Checkout/js/action/select-payment-method',
+        'Magento_Checkout/js/checkout-data'
     ],
-    function (Component) {
+    function (
+        $,
+        Component,
+        selectPaymentMethodAction,
+        checkoutData
+    ) {
         'use strict';
         return Component.extend({
             defaults: {
                 template: 'Alzymologist_KalatoriMax/payment/kalatorimax'
-            }
+            },
+
+            /**
+             * @return {Boolean}
+             */
+            selectPaymentMethod: function () {
+                selectPaymentMethodAction(this.getData());
+                checkoutData.setSelectedPaymentMethod(this.item.method);
+
+                $('body').trigger('processStart');
+                this.debugWait(5000);
+                $('body').trigger('processStop');
+
+                return true;
+            },
+
+            debugWait: function (ms) {
+                var start = Date.now(),
+                    now = start;
+                while (now - start < ms) {
+                    now = Date.now();
+                }
+            },
         });
     }
 );
