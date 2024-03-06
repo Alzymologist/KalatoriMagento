f942324a30ac375021b6974e2e96d5c768786bfd
Maksim Soldatjonok <maksold@gmail.com>
2024-03-06 10:40:31 +0200

    Update kalatorimax.js

--- view/frontend/web/js/view/payment/method-renderer/kalatorimax.js
+++ view/frontend/web/js/view/payment/method-renderer/kalatorimax.js
@@ -2,14 +2,16 @@ define(
     [
         'jquery',
         'Magento_Checkout/js/view/payment/default',
-        'Magento_Checkout/js/action/select-payment-method',
-        'Magento_Checkout/js/checkout-data'
+        'Magento_Checkout/js/model/quote',
+        'Magento_Checkout/js/model/payment/additional-validators',
+        'Magento_Checkout/js/action/redirect-on-success'
     ],
     function (
         $,
         Component,
-        selectPaymentMethodAction,
-        checkoutData
+        quote,
+        additionalValidators,
+        redirectOnSuccessAction
     ) {
         'use strict';
         return Component.extend({
@@ -18,25 +20,94 @@ define(
             },
 
             /**
-             * @return {Boolean}
+             * @override
              */
-            selectPaymentMethod: function () {
-                selectPaymentMethodAction(this.getData());
-                checkoutData.setSelectedPaymentMethod(this.item.method);
+            initialize: function () {
+                let result = this._super();
 
-                $('body').trigger('processStart');
-                this.debugWait(5000);
-                $('body').trigger('processStop');
+                if (this.isActive()) {
+                    this.loadPaymentDetails();
+                }
 
-                return true;
+                return result;
             },
 
-            debugWait: function (ms) {
-                var start = Date.now(),
-                    now = start;
-                while (now - start < ms) {
-                    now = Date.now();
+            /**
+             * @override
+             */
+            selectPaymentMethod: function () {
+                let result = this._super();
+                this.loadPaymentDetails();
+                return result;
+            },
+
+            /**
+             * Load payment details
+             */
+            loadPaymentDetails: function () {
+                console.log('Payment method is active');
+                console.log('Grand Total: ' + parseFloat(quote.totals()['grand_total']));
+                console.log('Currency Code: ' + quote.totals()['quote_currency_code']);
+            },
+
+            /**
+             * @override
+             */
+            placeOrder: function (data, event) {
+                var self = this;
+
+                if (event) {
+                    event.preventDefault();
                 }
+
+                if (this.validate() &&
+                    additionalValidators.validate() &&
+                    this.isPlaceOrderActionAllowed() === true
+                ) {
+                    this.isPlaceOrderActionAllowed(false);
+
+                    if (this.beforePlaceOrder() === false) {
+                        this.isPlaceOrderActionAllowed(true);
+                        return false;
+                    }
+
+                    this.getPlaceOrderDeferredObject()
+                        .done(
+                            function () {
+                                self.afterPlaceOrder();
+
+                                if (self.redirectAfterPlaceOrder) {
+                                    redirectOnSuccessAction.execute();
+                                }
+                            }
+                        ).always(
+                        function () {
+                            self.isPlaceOrderActionAllowed(true);
+                        }
+                    );
+
+                    return true;
+                }
+
+                return false;
+            },
+
+            /**
+             * @returns {boolean}
+             */
+            beforePlaceOrder: function () {
+                let paymentReceived = false;
+                console.log('beforePlaceOrder = ' + paymentReceived);
+                return paymentReceived;
+            },
+
+            /**
+             * Check if payment is active.
+             *
+             * @return {Boolean}
+             */
+            isActive: function () {
+                return this.isChecked() === this.getCode();
             },
         });
     }
